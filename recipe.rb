211263228a9a7ba96b2bb7fde25280ae2b81fd15
taroooyan <taroooyan@gmail.com>
2017-02-04 23:10:09 +0900

    delete debug line

--- recipe.rb
+++ recipe.rb
@@ -1,5 +1,3 @@
-include_recipe './cookbooks/hub/default.rb'
-return
 # package
 include_recipe './cookbooks/vim/default.rb'
 include_recipe './cookbooks/zsh/default.rb'
@@ -14,3 +12,4 @@ include_recipe './cookbooks/dotfiles/default.rb'
 include_recipe './cookbooks/go/default.rb'
 include_recipe './cookbooks/ghq/default.rb'
 include_recipe './cookbooks/peco/default.rb'
+include_recipe './cookbooks/hub/default.rb'
